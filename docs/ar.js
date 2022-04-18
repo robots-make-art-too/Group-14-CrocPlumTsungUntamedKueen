e8e22f6e1331dd876fa0ab50e2eb784ef56dc616
DhruvVal <90467684+DhruvVal@users.noreply.github.com>
2022-04-18 14:44:32 -0400

    test

--- docs/ar.js
+++ docs/ar.js
@@ -1,10 +1,59 @@
-window.onload = function () {
-    document
-      .querySelector(".say-hi-button")
-      .addEventListener("click", function () {
-        // here you can change also a-scene or a-entity properties, like
-        // changing your 3D model source, size, position and so on
-        // or you can just open links, trigger actions...
-        alert("Hi there!");
-      });
-  };
+window.onload = () => {
+  const button = document.querySelector('button[data-action="change"]');
+  button.innerText = '﹖';
+}
+var models = [
+  {
+    url: 'dirt.gltf',
+    scale: '0.05 0.05 0.05',
+    rotation: '0 0 0',
+  },
+  {
+      url: 'Seed.gltf',
+      scale: '0.05 0.05 0.05',
+      rotation: '0 0 0',
+    
+  },
+  {
+      url: 'SunFlower.gltf',
+      scale: '0.05 0.05 0.05',
+      rotation: '0 0 0',
+  },
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
+
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
+
+function renderPlaces(places) {
+  let scene = document.querySelector('a-scene');
+
+  setModel(models[modelIndex], model);
+
+  model.setAttribute('animation-mixer', '');
+
+ document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+ var newIndex = modelIndex % models.length;
+ setModel(models[newIndex], entity);
+});
+
+scene.appendChild(model);
+};
+
+
